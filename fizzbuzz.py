51e4325ce61a9792312a8cdbb9d4187fda536aef
donh3-test <139598701+donh3-test@users.noreply.github.com>
2023-07-15 00:46:31 +0000

    ths is a message.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ for i in range(1, 101):
     divisible_by_three = i % 3 == 0
     divisible_by_five = i % 5 == 0
     if(divisible_by_three & divisible_by_five):
-        print('FizzyBuzz')
+        print('FizzBuzz')
     elif(divisible_by_three):
         print('Fizz')
     elif(divisible_by_five):
